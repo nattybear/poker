8032d8882a093ff964f4665d305a742f29c6b4d1
준규 <vpark45@gmail.com>
2022-02-18 01:20:20 +0900

    Make categorize and lengths helpers function to findCategory

--- src/Hand.hs
+++ src/Hand.hs
@@ -27,16 +27,16 @@ groups :: Hand -> [[Rank]]
 groups = sortBy (descending (comparing lg))
          . group . sort . map rank
 
-lengths :: [[a]] -> [Int]
-lengths = map length
-
-categorize :: [Int] -> Category
-categorize [1,1,1,1,1] = HighCard
-categorize [2,1,1,1]   = OnePair
-categorize [2,2,1]     = TwoPairs
-categorize [3,1,1]     = ThreeOfAKind
-categorize [3,2]       = FullHouse
-categorize [4,1]       = FourOfAKind
-
 findCategory :: Hand -> Category
 findCategory = categorize . lengths . groups
+  where
+    categorize :: [Int] -> Category
+    categorize [1,1,1,1,1] = HighCard
+    categorize [2,1,1,1]   = OnePair
+    categorize [2,2,1]     = TwoPairs
+    categorize [3,1,1]     = ThreeOfAKind
+    categorize [3,2]       = FullHouse
+    categorize [4,1]       = FourOfAKind
+
+    lengths :: [[a]] -> [Int]
+    lengths = map length
