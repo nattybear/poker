e4f923c1ad39f2509bf4d056eb564e116fe4c64f
준규 <vpark45@gmail.com>
2022-02-18 01:42:39 +0900

    Attach helper functions to the public functions

--- src/Hand.hs
+++ src/Hand.hs
@@ -6,8 +6,6 @@ import Data.Ord
 
 type Hand = [Card]
 
-type Ranking = (Category, [Rank])
-
 data Category
   = HighCard
   | OnePair
@@ -21,35 +19,37 @@ data Category
   | RoyalFlush
   deriving (Eq,Ord)
 
-lg :: [a] -> (Int, [a])
-lg xs = (length xs, xs)
-
-descending :: (a -> b -> c) -> b -> a -> c
-descending = flip
-
-groups :: Hand -> [[Rank]]
-groups = sortBy (descending (comparing lg))
-         . group . sort . map rank
-
-findCategory :: Hand -> Category
-findCategory = categorize . lengths . groups
-  where
-    categorize :: [Int] -> Category
-    categorize [1,1,1,1,1] = HighCard
-    categorize [2,1,1,1]   = OnePair
-    categorize [2,2,1]     = TwoPairs
-    categorize [3,1,1]     = ThreeOfAKind
-    categorize [3,2]       = FullHouse
-    categorize [4,1]       = FourOfAKind
-
-    lengths :: [[a]] -> [Int]
-    lengths = map length
-
-ranking :: Hand -> Ranking
-ranking h = (findCategory h, concat (groups h)) 
+type Ranking = (Category, [Rank])
 
 category :: Ranking -> Category
 category = fst
 
 ranks :: Ranking -> [Rank]
 ranks = snd
+
+ranking :: Hand -> Ranking
+ranking h = (findCategory h, concat (groups h)) 
+  where
+    findCategory :: Hand -> Category
+    findCategory = categorize . lengths . groups
+      where
+        categorize :: [Int] -> Category
+        categorize [1,1,1,1,1] = HighCard
+        categorize [2,1,1,1]   = OnePair
+        categorize [2,2,1]     = TwoPairs
+        categorize [3,1,1]     = ThreeOfAKind
+        categorize [3,2]       = FullHouse
+        categorize [4,1]       = FourOfAKind
+
+        lengths :: [[a]] -> [Int]
+        lengths = map length
+
+    groups :: Hand -> [[Rank]]
+    groups = sortBy (descending (comparing lg))
+             . group . sort . map rank
+      where
+        lg :: [a] -> (Int, [a])
+        lg xs = (length xs, xs)
+
+        descending :: (a -> b -> c) -> b -> a -> c
+        descending = flip
