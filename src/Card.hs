2ef49b44e336b2d94646718cd7acfe8d8b4e4ece
준규 <vpark45@gmail.com>
2022-02-17 21:02:59 +0900

    Rewrite String to [Char]

--- src/Card.hs
+++ src/Card.hs
@@ -10,14 +10,14 @@ suit :: Card -> Suit
 suit = snd
 
 card :: String -> Card
-card "J♥" = (Jack, Hearts)
-card "Q♥" = (Queen, Hearts)
-card "K♥" = (King, Hearts)
-card "A♥" = (Ace, Hearts)
-card "A♠" = (Ace, Spades)
-card "A♦" = (Ace, Diamonds)
-card "A♣" = (Ace, Clubs)
-card "K♣" = (King, Clubs)
+card ['J', '♥'] = (Jack, Hearts)
+card ['Q', '♥'] = (Queen, Hearts)
+card ['K', '♥'] = (King, Hearts)
+card ['A', '♥'] = (Ace, Hearts)
+card ['A', '♠'] = (Ace, Spades)
+card ['A', '♦'] = (Ace, Diamonds)
+card ['A', '♣'] = (Ace, Clubs)
+card ['K', '♣'] = (King, Clubs)
 
 cards :: String -> [Card]
 cards = map card . words
