1e7b7bd75b3034b6421a81f7b721f36dd090dd8d
준규 <vpark45@gmail.com>
2022-02-19 19:37:54 +0900

    Insert qualify to ranking function

--- src/Hand.hs
+++ src/Hand.hs
@@ -28,7 +28,8 @@ ranks :: Ranking -> [Rank]
 ranks = snd
 
 ranking :: Hand -> Ranking
-ranking h = promote (findCategory h, concat (groups h)) 
+ranking h = qualify (isFlush h)
+            (promote (findCategory h, concat (groups h)))
   where
     findCategory :: Hand -> Category
     findCategory = categorize . lengths . groups
