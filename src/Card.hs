ea2e6042897961a0949b9acbc8c49b21f63bddab
준규 <vpark45@gmail.com>
2022-02-19 19:08:02 +0900

    Modify indentation

--- src/Card.hs
+++ src/Card.hs
@@ -6,20 +6,20 @@ data Suit = Hearts | Spades | Diamonds | Clubs
   deriving Eq
 
 data Rank
- = Two
- | Three
- | Four
- | Five
- | Six
- | Seven
- | Eight
- | Nine
- | Ten
- | Jack
- | Queen
- | King
- | Ace
- deriving (Eq,Ord,Enum)
+  = Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace
+  deriving (Eq,Ord,Enum)
 
 rank :: Card -> Rank
 rank = fst
