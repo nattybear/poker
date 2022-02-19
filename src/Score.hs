1a6cac3e35610bac91dd7c803b361ab822507a57
준규 <vpark45@gmail.com>
2022-02-19 21:13:58 +0900

    Add findRanking function

--- src/Score.hs
+++ src/Score.hs
@@ -9,3 +9,8 @@ choose n = filter ((==n) . length) . subsequences
 
 bestRanking :: [Card] -> Ranking
 bestRanking = maximum . map ranking . choose 5
+
+findRanking :: [Card] -> Maybe Ranking
+findRanking cs
+  | length cs /= 7 = Nothing
+  | otherwise = Just (bestRanking cs)
