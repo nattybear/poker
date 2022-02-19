d087c049013f11f6155528b544496047febacdb4
준규 <vpark45@gmail.com>
2022-02-19 21:10:21 +0900

    Add bestRanking function

--- src/Score.hs
+++ src/Score.hs
@@ -1,6 +1,11 @@
 module Score where
 
+import Card
 import Data.List
+import Hand
 
 choose :: Int -> [a] -> [[a]]
 choose n = filter ((==n) . length) . subsequences
+
+bestRanking :: [Card] -> Ranking
+bestRanking = maximum . map ranking . choose 5
